3f9f16232ba90f9c60bb9c8ad35159bb407be054
mdbarrus <michael.barrus@gmail.com>
2018-06-22 16:26:56 -0400

    sitemap links update
    used absolute links instead of relative; also updated governance links to new names and directory structure

--- sitemap.php
+++ sitemap.php
@@ -82,67 +82,67 @@ hidediv('sitemap');
 			<TR>
 				<TD VALIGN="top">  <!--  Home News Etc. Start -->
 					<UL class="sitemap">
-						<li><A HREF="index.php">Home</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/index.php">Home</A></li>
 						<HR class="rightindent">
-						<li><A HREF="news.php">Main News Page</A></li>
-						<li><A HREF="newsletter.php">Current Newsletter</A></li>
-						<li><A HREF="past_newsletters.php">Newsletter Archive</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/news.php">Main News Page</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/newsletter.php">Current Newsletter</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/past_newsletters.php">Newsletter Archive</A></li>
 						<HR class="rightindent">
-						<li><A HREF="links.php">Links</A></li>
-						<li><a href="search.php" class="dir">Search</a></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/links.php">Links</A></li>
+						<li><a href="http://sections.maa.org/northeastern/newsite/search.php" class="dir">Search</a></li>
 					</UL>
 				</TD> <!--  Home News End -->
 				<TD VALIGN="top">    <!--  Governance Start -->
 					<UL class="sitemap">
-						<li><A HREF="SectionOfficers.php">Section Officers</A></li>
-						<li><A HREF="Bylaws.php">Section Bylaws</A></li>
-						<li><A HREF="ReportsMinutes.php">Reports and Minutes</A></li>
-						<li><A HREF="Forms.php">Forms</A></li>
-						<li><A HREF="Guides.php">Guides</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/gov/officers.php">Section Officers</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/gov/bylaws.pdf">Section Bylaws</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/gov/reports_and_minutes.php">Reports and Minutes</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/gov/forms.php">Forms</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/gov/guides.php">Guides</A></li>
 					</UL>
 				</TD>   <!--  Governance End -->
 				<TD VALIGN="top">   <!--  Meetings Begin -->
 					<ul class="sitemap">
-						<li><A HREF="meetings.php">Next Section Meeting</A></li> <!-- Replace this with a link to the Google Sites page when a conference website is ready! -->
-						<li><A HREF="FutureMeetings.php">Future Section Meetings</A></li>
-						<li><A HREF="PastMeetings.php">Past Section Meetings</A></li>
-						<li><A HREF="DinnerMeetings.php">Dinner Meetings</A></li>
-						<li><A HREF="MiniCourses.php">Mini-Courses</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/meetings.php">Next Section Meeting</A></li> <!-- Replace this with a link to the Google Sites page when a conference website is ready! -->
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/FutureMeetings.php">Future Section Meetings</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/PastMeetings.php">Past Section Meetings</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/DinnerMeetings.php">Dinner Meetings</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/MiniCourses.php">Mini-Courses</A></li>
 					</ul>
 				</TD>   <!--  Meetings End -->
 				<TD VALIGN="top">   <!--  Student Activities Begin -->
 					<UL class="sitemap">
-						<li><A HREF="Competitions.php">Competitions</A></li>
-						<li><A HREF="StudentPaperSessions.php">Student Paper Sessions</A></li>
-						<li><A HREF="StudentConferences.php">Student Conferences</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/Competitions.php">Competitions</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/StudentPaperSessions.php">Student Paper Sessions</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/StudentConferences.php">Student Conferences</A></li>
 					</UL>
 				</TD>   <!--  Student Activities End -->
 				<TD VALIGN="top">   <!--  Section NExT Begin -->
 					<UL class="sitemap">
-						<li><A HREF="sectionnext.php">About Section NExT (New Experiences in Teaching)</A></li>
-						<li><A HREF="NExTNews.php">Current Section NExT News</A></li>
-						<li><A HREF="NExTpast.php">Previous Section NExT Meetings</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/sectionnext.php">About Section NExT (New Experiences in Teaching)</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/NExTNews.php">Current Section NExT News</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/NExTpast.php">Previous Section NExT Meetings</A></li>
 					</UL>
 				</TD>   <!--  Section NExT End -->
 				<TD VALIGN="top">   <!--  History Begin -->
 					<UL class="sitemap">
-						<li><A HREF="history.php">Introduction</A></li>
-						<li><A HREF="ChristieBio.php">Dan Edwin Christie (1915-1975)</A></li>
-						<li><A HREF="EvesBio.php">Howard Eves (1911-2004)</A></li>
-						<li><A HREF="BattlesBio.php">Francis P. Battles (1941-2015)</A></li>
-						<li><A HREF="Decades.php">History by Decade</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/history.php">Introduction</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/ChristieBio.php">Dan Edwin Christie (1915-1975)</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/EvesBio.php">Howard Eves (1911-2004)</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/BattlesBio.php">Francis P. Battles (1941-2015)</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/Decades.php">History by Decade</A></li>
 						<HR class="rightindent">
-						<li><A HREF="BattlesLectures.php">Battles Lectures</A></li>
-						<li><A HREF="ChristieLectures.php">Christie Lectures</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/BattlesLectures.php">Battles Lectures</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/ChristieLectures.php">Christie Lectures</A></li>
 					</UL>
 				</TD>   <!--  History End -->
 				<TD VALIGN="top">   <!--  Awards Begin -->
 					<UL class="sitemap">
-						<li><A HREF="Awards.php">Main Awards Page</A></li>
-						<li><A HREF="DistinguishedTeachingAward.php">Award for Distinguished College or University Teaching</A></li>
-						<li><A HREF="EvesAward.php">Howard Eves Award</A></li>
-						<li><A HREF="MeritoriousServiceAward.php">MAA Certificate for Meritorious Service</A></li>
-						<li><A HREF="nominations.php">Nominate Someone</a></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/Awards.php">Main Awards Page</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/DistinguishedTeachingAward.php">Award for Distinguished College or University Teaching</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/EvesAward.php">Howard Eves Award</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/MeritoriousServiceAward.php">MAA Certificate for Meritorious Service</A></li>
+						<li><A HREF="http://sections.maa.org/northeastern/newsite/nominations.php">Nominate Someone</a></li>
 					</UL>
 				</TD>   <!--  Awards End -->
 			</TR>
@@ -154,6 +154,6 @@ hidediv('sitemap');
 	</div>
 	<div class=copyright>
 		<HR>
-		Copyright &copy; 2017 - Northeastern Section of the Mathematical Association of America<BR>Please send comments, suggestions, or corrections for this page to the webmaster at  <A HREF='mailto:northeasternsection@gmail.com'>northeasternsection@gmail.com</A><BR>Last Modified:  09/7/2017 - 09:34am
+		Copyright &copy; 2018 - Northeastern Section of the Mathematical Association of America<BR>Please send comments, suggestions, or corrections for this page to the webmaster at  <A HREF='mailto:northeasternsection@gmail.com'>northeasternsection@gmail.com</A><BR>Last Modified:  06/22/2018 - 04:20pm
 	</div>
 </div>
