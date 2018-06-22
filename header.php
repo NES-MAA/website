1f756002891b13aca33ce0ed673003600e3b2aff
osj1961 <fieldsj1@southernct.edu>
2018-06-22 16:37:10 -0400

    fixes

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 
 		<!-- Beginning of compulsory dropdown code below -->
-		<link href="${prefix}css/dropdown/dropdown.css" media="screen" rel="stylesheet" type="text/css" />
-		<link href="${prefix}css/dropdown/themes/nvidia.com/default.advanced.css" media="screen" rel="stylesheet" type="text/css" />
+		<link href="<?php echo ${prefix} ?>css/dropdown/dropdown.css" media="screen" rel="stylesheet" type="text/css" />
+		<link href="<?php echo ${prefix} ?>css/dropdown/themes/nvidia.com/default.advanced.css" media="screen" rel="stylesheet" type="text/css" />
 		<!-- End of dropdown code -->
 		<CENTER>
 		    <div class="frame">
@@ -12,7 +12,7 @@
 				<h1 class=ESMC2 width=100%>
     				<table class=HeaderTable>
 	    			    <TR>
-		    			    <TD><A HREF="http://www.maa.org" target="_blank"><IMG SRC="${prefix}images/Round_MAA_logo_white_2.gif" border=0></A></TD>
+		    			    <TD><A HREF="http://www.maa.org" target="_blank"><IMG SRC="<?php echo ${prefix} ?>images/Round_MAA_logo_white_2.gif" border=0></A></TD>
 			    			<TD class=pad>The Mathematical Association of America<BR>Northeastern Section</TD>
 				    	</TR>
 				    </table>
@@ -24,70 +24,70 @@
 				<!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! HORIZONTAL MENU BAR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
 				<!-- Beginning of compulsory dropdown code below -->
 				<ul class="dropdown dropdown-horizontal">
-				    <li><a href="${prefix}index.php" class="dir">Home</a></li>
-					<li><a href="${prefix}news.php" class="dir">News</a>
+				    <li><a href="<?php echo ${prefix} ?>index.php" class="dir">Home</a></li>
+					<li><a href="<?php echo ${prefix} ?>news.php" class="dir">News</a>
 						<UL>
-						    <li><A HREF="${prefix}news.php">Main News Page</A></li>
-							<li><A HREF="${prefix}newsletter.php">Current Newsletter</A></li>
-							<li><A HREF="${prefix}newsletter-archive.php">Newsletter Archive</A></li>
+						    <li><A HREF="<?php echo ${prefix} ?>news.php">Main News Page</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>newsletter.php">Current Newsletter</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>newsletter-archive.php">Newsletter Archive</A></li>
 							<!-- This is for a horizontal divider line in dropdown menus: <HR class=leftmenu>	-->
 						</ul>
 					</li>
-					<li><a href="${prefix}gov/index.php" class="dir">Governance</a>
+					<li><a href="<?php echo ${prefix} ?>gov/index.php" class="dir">Governance</a>
 						<ul>
-							<li><A HREF="${prefix}gov/officers.php">Section Officers</A></li>
-							<li><A HREF="${prefix}gov/bylaws.pdf">Section Bylaws</A></li>
-							<li><A HREF="${prefix}gov/reports_and_minutes.php">Reports and Minutes</A></li>
-							<li><A HREF="${prefix}gov/forms.php">Forms</A></li>
-							<li><A HREF="${prefix}gov/guides.php">Guides</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>gov/officers.php">Section Officers</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>gov/bylaws.pdf">Section Bylaws</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>gov/reports_and_minutes.php">Reports and Minutes</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>gov/forms.php">Forms</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>gov/guides.php">Guides</A></li>
 						</ul>
 					</li>
-					<li><a href="${prefix}meetings.php" class="dir">Meetings</a>
+					<li><a href="<?php echo ${prefix} ?>meetings.php" class="dir">Meetings</a>
 						<ul>
-							<li><A HREF="${prefix}meetings.php">Next Section Meeting</A></li> <!-- Replace this with a link to the Google Sites page when a conference website is ready! -->
-							<li><A HREF="${prefix}FutureMeetings.php">Future Section Meetings</A></li>
-							<li><A HREF="${prefix}PastMeetings.php">Past Section Meetings</A></li>
-							<li><A HREF="${prefix}DinnerMeetings.php">Dinner Meetings</A></li>
-							<li><A HREF="${prefix}MiniCourses.php">Mini-Courses</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>meetings.php">Next Section Meeting</A></li> <!-- Replace this with a link to the Google Sites page when a conference website is ready! -->
+							<li><A HREF="<?php echo ${prefix} ?>FutureMeetings.php">Future Section Meetings</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>PastMeetings.php">Past Section Meetings</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>DinnerMeetings.php">Dinner Meetings</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>MiniCourses.php">Mini-Courses</A></li>
 						</ul>
 					</li>
-					<li><a href="${prefix}StudentActivities.php" class="dir">Student Activities</a>
+					<li><a href="<?php echo ${prefix} ?>StudentActivities.php" class="dir">Student Activities</a>
 						<ul>
-							<li><A HREF="${prefix}Competitions.php">Competitions</A></li>
-							<li><A HREF="${prefix}StudentPaperSessions.php">Student Paper Sessions</A></li>
-							<li><A HREF="${prefix}StudentConferences.php">Student Conferences</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>Competitions.php">Competitions</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>StudentPaperSessions.php">Student Paper Sessions</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>StudentConferences.php">Student Conferences</A></li>
 						</ul>
 					</li>
-					<li><a href="${prefix}sectionnext.php" class="dir">Section&nbsp;NExT</a>
+					<li><a href="<?php echo ${prefix} ?>sectionnext.php" class="dir">Section&nbsp;NExT</a>
 						<ul>
-							<li><A HREF="${prefix}sectionnext.php">About Section NExT (New Experiences in Teaching)</A></li>
-							<li><A HREF="${prefix}NExTNews.php">Current Section NExT News</A></li>
-							<li><A HREF="${prefix}NExTpast.php">Previous Section NExT Meetings</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>sectionnext.php">About Section NExT (New Experiences in Teaching)</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>NExTNews.php">Current Section NExT News</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>NExTpast.php">Previous Section NExT Meetings</A></li>
 						</ul>
 					</li>
-					<li><a href="${prefix}history.php" class="dir">History</a>
+					<li><a href="<?php echo ${prefix} ?>history.php" class="dir">History</a>
 						<ul>
-							<li><A HREF="${prefix}history.php">Introduction</A></li>
-							<li><A HREF="${prefix}ChristieBio.php">Dan Edwin Christie (1915-1975)</A></li>
-							<li><A HREF="${prefix}EvesBio.php">Howard Eves (1911-2004)</A></li>
-							<li><A HREF="${prefix}BattlesBio.php">Francis P. Battles (1941-2015)</A></li>
-							<li><A HREF="${prefix}Decades.php">History by Decade</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>history.php">Introduction</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>ChristieBio.php">Dan Edwin Christie (1915-1975)</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>EvesBio.php">Howard Eves (1911-2004)</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>BattlesBio.php">Francis P. Battles (1941-2015)</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>Decades.php">History by Decade</A></li>
 							<HR class=leftmenu>
-							<li><A HREF="${prefix}BattlesLectures.php">Battles Lectures</A></li>
-							<li><A HREF="${prefix}ChristieLectures.php">Christie Lectures</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>BattlesLectures.php">Battles Lectures</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>ChristieLectures.php">Christie Lectures</A></li>
 						</ul>
 					</li>
-					<li><a href="${prefix}awards.php" class="dir">Awards</a>
+					<li><a href="<?php echo ${prefix} ?>awards.php" class="dir">Awards</a>
 						<ul>
-							<li><A HREF="${prefix}Awards.php">Main Awards Page</A></li>
-							<li><A HREF="${prefix}DistinguishedTeachingAward.php">Award for Distinguished College or University Teaching</A></li>
-							<li><A HREF="${prefix}EvesAward.php">Howard Eves Award</A></li>
-							<li><A HREF="${prefix}MeritoriousServiceAward.php">MAA Certificate for Meritorious Service</A></li>
-							<li><A HREF="${prefix}nominations.php">Nominate Someone</a></li>
+							<li><A HREF="<?php echo ${prefix} ?>Awards.php">Main Awards Page</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>DistinguishedTeachingAward.php">Award for Distinguished College or University Teaching</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>EvesAward.php">Howard Eves Award</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>MeritoriousServiceAward.php">MAA Certificate for Meritorious Service</A></li>
+							<li><A HREF="<?php echo ${prefix} ?>nominations.php">Nominate Someone</a></li>
 						</ul>
 					</li>
-					<li><a href="${prefix}links.php" class="dir">Links</a></li>
-					<li class="last"><a href="${prefix}search.php" class="dir">Search</a></li>
+					<li><a href="<?php echo ${prefix} ?>links.php" class="dir">Links</a></li>
+					<li class="last"><a href="<?php echo ${prefix} ?>search.php" class="dir">Search</a></li>
 				</ul>
 			</div>
 		</center>
