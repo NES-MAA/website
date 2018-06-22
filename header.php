c157ba392c08bb4c9ee469df2bbf2e9f2f1d7313
mdbarrus <michael.barrus@gmail.com>
2018-06-22 14:29:38 -0400

    complete absolute links for header
    Changing all remaining relative links to absolute links in header.php

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 				<h1 class=ESMC2 width=100%>
     				<table class=HeaderTable>
 	    			    <TR>
-		    			    <TD><A HREF="http://www.maa.org" target="_blank"><IMG SRC="images/Round_MAA_logo_white_2.gif" border=0></A></TD>
+		    			    <TD><A HREF="http://www.maa.org" target="_blank"><IMG SRC="http://sections.maa.org/northeastern/newsite/images/Round_MAA_logo_white_2.gif" border=0></A></TD>
 			    			<TD class=pad>The Mathematical Association of America<BR>Northeastern Section</TD>
 				    	</TR>
 				    </table>
@@ -24,12 +24,12 @@
 				<!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! HORIZONTAL MENU BAR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
 				<!-- Beginning of compulsory dropdown code below -->
 				<ul class="dropdown dropdown-horizontal">
-				    <li><a href="index.php" class="dir">Home</a></li>
-					<li><a href="news.php" class="dir">News</a>
+				    <li><a href="http://sections.maa.org/northeastern/newsite/index.php" class="dir">Home</a></li>
+					<li><a href="http://sections.maa.org/northeastern/newsite/news.php" class="dir">News</a>
 						<UL>
-						    <li><A HREF="news.php">Main News Page</A></li>
-							<li><A HREF="newsletter.php">Current Newsletter</A></li>
-							<li><A HREF="newsletter-archive.php">Newsletter Archive</A></li>
+						    <li><A HREF="http://sections.maa.org/northeastern/newsite/news.php">Main News Page</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/newsletter.php">Current Newsletter</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/newsletter-archive.php">Newsletter Archive</A></li>
 							<!-- This is for a horizontal divider line in dropdown menus: <HR class=leftmenu>	-->
 						</ul>
 					</li>
@@ -42,52 +42,52 @@
 							<li><A HREF="http://sections.maa.org/northeastern/newsite/gov/guides.php">Guides</A></li>
 						</ul>
 					</li>
-					<li><a href="meetings.php" class="dir">Meetings</a>
+					<li><a href="http://sections.maa.org/northeastern/newsite/meetings.php" class="dir">Meetings</a>
 						<ul>
-							<li><A HREF="meetings.php">Next Section Meeting</A></li> <!-- Replace this with a link to the Google Sites page when a conference website is ready! -->
-							<li><A HREF="FutureMeetings.php">Future Section Meetings</A></li>
-							<li><A HREF="PastMeetings.php">Past Section Meetings</A></li>
-							<li><A HREF="DinnerMeetings.php">Dinner Meetings</A></li>
-							<li><A HREF="MiniCourses.php">Mini-Courses</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/meetings.php">Next Section Meeting</A></li> <!-- Replace this with a link to the Google Sites page when a conference website is ready! -->
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/FutureMeetings.php">Future Section Meetings</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/PastMeetings.php">Past Section Meetings</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/DinnerMeetings.php">Dinner Meetings</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/MiniCourses.php">Mini-Courses</A></li>
 						</ul>
 					</li>
-					<li><a href="StudentActivities.php" class="dir">Student Activities</a>
+					<li><a href="http://sections.maa.org/northeastern/newsite/StudentActivities.php" class="dir">Student Activities</a>
 						<ul>
-							<li><A HREF="Competitions.php">Competitions</A></li>
-							<li><A HREF="StudentPaperSessions.php">Student Paper Sessions</A></li>
-							<li><A HREF="StudentConferences.php">Student Conferences</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/Competitions.php">Competitions</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/StudentPaperSessions.php">Student Paper Sessions</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/StudentConferences.php">Student Conferences</A></li>
 						</ul>
 					</li>
-					<li><a href="sectionnext.php" class="dir">Section&nbsp;NExT</a>
+					<li><a href="http://sections.maa.org/northeastern/newsite/sectionnext.php" class="dir">Section&nbsp;NExT</a>
 						<ul>
-							<li><A HREF="sectionnext.php">About Section NExT (New Experiences in Teaching)</A></li>
-							<li><A HREF="NExTNews.php">Current Section NExT News</A></li>
-							<li><A HREF="NExTpast.php">Previous Section NExT Meetings</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/sectionnext.php">About Section NExT (New Experiences in Teaching)</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/NExTNews.php">Current Section NExT News</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/NExTpast.php">Previous Section NExT Meetings</A></li>
 						</ul>
 					</li>
-					<li><a href="history.php" class="dir">History</a>
+					<li><a href="http://sections.maa.org/northeastern/newsite/history.php" class="dir">History</a>
 						<ul>
-							<li><A HREF="history.php">Introduction</A></li>
-							<li><A HREF="ChristieBio.php">Dan Edwin Christie (1915-1975)</A></li>
-							<li><A HREF="EvesBio.php">Howard Eves (1911-2004)</A></li>
-							<li><A HREF="BattlesBio.php">Francis P. Battles (1941-2015)</A></li>
-							<li><A HREF="Decades.php">History by Decade</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/history.php">Introduction</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/ChristieBio.php">Dan Edwin Christie (1915-1975)</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/EvesBio.php">Howard Eves (1911-2004)</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/BattlesBio.php">Francis P. Battles (1941-2015)</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/Decades.php">History by Decade</A></li>
 							<HR class=leftmenu>
-							<li><A HREF="BattlesLectures.php">Battles Lectures</A></li>
-							<li><A HREF="ChristieLectures.php">Christie Lectures</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/BattlesLectures.php">Battles Lectures</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/ChristieLectures.php">Christie Lectures</A></li>
 						</ul>
 					</li>
-					<li><a href="awards.php" class="dir">Awards</a>
+					<li><a href="http://sections.maa.org/northeastern/newsite/awards.php" class="dir">Awards</a>
 						<ul>
-							<li><A HREF="Awards.php">Main Awards Page</A></li>
-							<li><A HREF="DistinguishedTeachingAward.php">Award for Distinguished College or University Teaching</A></li>
-							<li><A HREF="EvesAward.php">Howard Eves Award</A></li>
-							<li><A HREF="MeritoriousServiceAward.php">MAA Certificate for Meritorious Service</A></li>
-							<li><A HREF="nominations.php">Nominate Someone</a></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/Awards.php">Main Awards Page</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/DistinguishedTeachingAward.php">Award for Distinguished College or University Teaching</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/EvesAward.php">Howard Eves Award</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/MeritoriousServiceAward.php">MAA Certificate for Meritorious Service</A></li>
+							<li><A HREF="http://sections.maa.org/northeastern/newsite/nominations.php">Nominate Someone</a></li>
 						</ul>
 					</li>
-					<li><a href="links.php" class="dir">Links</a></li>
-					<li class="last"><a href="search.php" class="dir">Search</a></li>
+					<li><a href="http://sections.maa.org/northeastern/newsite/links.php" class="dir">Links</a></li>
+					<li class="last"><a href="http://sections.maa.org/northeastern/newsite/search.php" class="dir">Search</a></li>
 				</ul>
 			</div>
 		</center>
